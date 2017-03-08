b4deb1e70192dd56a8a6ea3a22a315dbb34c2e4c
Broc Gailit <brocgailit@gmail.com>
2017-03-07 20:01:08 -0800

    Implement vimeo service in press kit

--- src/press/index.ts
+++ src/press/index.ts
@@ -1,10 +1,15 @@
 import { inject } from 'aurelia-framework';
 import { FlickrService } from '../flickr.service';
+import { VimeoService } from '../vimeo.service';
 
-@inject(FlickrService)
+@inject(FlickrService, VimeoService)
 export class Press {
     
-    constructor(private flickr: FlickrService) {
-        flickr.getPublicPhotos();
+    photos: any;
+    videos: any;
+
+    constructor(private flickr: FlickrService, private vimeo: VimeoService) {
+        flickr.getPublicPhotos(10,1,'url_m').then(photos => { this.photos = photos});
+        vimeo.getPublicVideos().then(videos => { this.videos = videos; console.log(videos); });
     }
 }
