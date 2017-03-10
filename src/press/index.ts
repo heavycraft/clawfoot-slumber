7c2b6fb071762e1e486ad490dbaa8c32ae59cd55
Broc Gailit <brocgailit@gmail.com>
2017-03-10 14:35:20 -0800

    Add link to flickr pictures

--- src/press/index.ts
+++ src/press/index.ts
@@ -2,7 +2,7 @@ import { inject } from 'aurelia-framework';
 import { FlickrService } from '../flickr.service';
 import { VimeoService } from '../vimeo.service';
 import { SoundCloudService, ISoundCloudPlaylist } from '../soundcloud.service';
-
+import * as process from '../process.env';
 
 @inject(FlickrService, VimeoService, SoundCloudService)
 export class Press {
@@ -10,6 +10,7 @@ export class Press {
     photos: any;
     videos: any;
     playlist: ISoundCloudPlaylist;
+    flickrId = process.env.FLICKR_USER_ID;
 
     constructor(private flickr: FlickrService, private vimeo: VimeoService, private soundcloud: SoundCloudService) {
         flickr.getPublicPhotos(10,1,'url_m').then(photos => { this.photos = photos; });
