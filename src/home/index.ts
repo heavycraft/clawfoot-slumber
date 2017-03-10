a3faea5789d3c5b7e36c648b9d74b044a44a7bf7
Broc Gailit <brocgailit@gmail.com>
2017-03-10 13:48:42 -0800

    Use soundcloud for music section

--- src/home/index.ts
+++ src/home/index.ts
@@ -1,12 +1,13 @@
 import { inject } from 'aurelia-framework';
 import { BandsintownService } from '../bandsintown.service';
-import { SoundCloudService, ISoundCloudUser } from '../soundcloud.service';
+import { SoundCloudService, ISoundCloudUser, ISoundCloudPlaylist } from '../soundcloud.service';
 import { VimeoService, IVimeoVideo } from '../vimeo.service';
 
 @inject(BandsintownService, SoundCloudService, VimeoService)
 export class Home {
 
     band: ISoundCloudUser;
+    album: ISoundCloudPlaylist;
     video: IVimeoVideo;
     events = { past: [], future: [] };
     
@@ -15,6 +16,7 @@ export class Home {
         bandsintown.getEvents('past', 5).then(events => this.events.past = events);
         bandsintown.getEvents('upcoming').then(events => this.events.future = events);
         soundcloud.getUser('clawfootslumber').then(band => this.band = band);
+        soundcloud.getUserPlaylists('clawfootslumber').then(playlists => this.album = playlists[0]);
         vimeo.getPublicVideos().then(videos => this.video = videos[0]);
     }
 }
