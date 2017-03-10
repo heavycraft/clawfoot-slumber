ddbd90e62566a64abd422b5b18a2bfe393d51a6c
Broc Gailit <brocgailit@gmail.com>
2017-03-10 13:16:50 -0800

    Use soundcloud for band description

--- src/home/index.ts
+++ src/home/index.ts
@@ -1,14 +1,17 @@
 import { inject } from 'aurelia-framework';
 import { BandsintownService } from '../bandsintown.service';
+import { SoundCloudService, ISoundCloudUser } from '../soundcloud.service';
 
-@inject(BandsintownService)
+@inject(BandsintownService, SoundCloudService)
 export class Home {
 
+    band: ISoundCloudUser;
     events = { past: [], future: [] };
     
-    constructor(private bandsintown: BandsintownService) {
+    constructor(private bandsintown: BandsintownService, private soundcloud: SoundCloudService) {
         bandsintown.configure({ artistname: 'ClawfootSlumber', app_id: 'CLAWFOOT_SLUMBER' });
         bandsintown.getEvents('past', 5).then(events => this.events.past = events);
         bandsintown.getEvents('upcoming').then(events => this.events.future = events);
+        soundcloud.getUser('clawfootslumber').then(band => this.band = band);
     }
 }
